ecf543ba21789319961926b72d4fe2d225bde60c
dang16062004 <phamcongdang16062004@gmail.com>
2024-10-13 20:36:34 +0700

    Thay đổi shopping cart

---
+++ Extensions/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace TestWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Thêm dịch vụ session
+            services.AddControllersWithViews();
+            services.AddSession(); // Thêm dòng này để sử dụng session
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            // Thêm middleware session
+            app.UseSession(); // Thêm dòng này để sử dụng session
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
